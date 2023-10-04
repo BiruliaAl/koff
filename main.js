8299d96d110a323cf715ae74dd7135a736d80b16
Alena Birulia <birulia.ip@gmail.com>
2023-10-04 22:02:41 +0300

    html shop koff

---
+++ main.js
@@ -0,0 +1,30 @@
+import 'normalize.css';
+import './style.scss';
+import { Navigation, Pagination } from 'swiper/modules';
+import Swiper from 'swiper';
+import 'swiper/css';
+
+
+// const swiper = new Swiper('.swiper', {
+//   // Optional parameters
+//   direction: 'vertical',
+//   loop: true,
+
+//   // If we need pagination
+//   pagination: {
+//     el: '.swiper-pagination',
+//   },
+
+//   // Navigation arrows
+//   navigation: {
+//     nextEl: '.product__arrow-next',
+//     prevEl: '.product__arrow-prev',
+//   },
+
+//   // And if we need scrollbar
+//   scrollbar: {
+//     el: '.swiper-scrollbar',
+//   },
+// });
+
+
